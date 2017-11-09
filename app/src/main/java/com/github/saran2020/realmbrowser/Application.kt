c20922e8202b25a5bf9ad935ab5a1e87955ecd26
Saran Sankaran <saran.sankaran@gmail.com>
2017-11-09 18:09:08 +0530

    Setup
    Signed-off-by: Saran Sankaran <saran.sankarn@gmail.com>

---
+++ app/src/main/java/com/github/saran2020/realmbrowser/Application.kt
@@ -0,0 +1,15 @@
+package com.github.saran2020.realmbrowser
+
+import android.app.Application
+import io.realm.Realm
+
+/**
+ * Created by Saran Sankaran on 11/9/17.
+ */
+class Application : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+    }
+}
--- app/src/main/java/com/github/saran2020/realmbrowser/MainActivity.kt
+++ app/src/main/java/com/github/saran2020/realmbrowser/MainActivity.kt
@@ -1,12 +1,46 @@
 package com.github.saran2020.realmbrowser
 
+import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import io.realm.Realm
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var realm: Realm
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        realm = Realm.getDefaultInstance()
+        var result = realm.where(RealmTestObject::class.java).findAll()
+
+        if (result.size == 0)
+            FeedDataTask().execute()
+    }
+
+    override fun onDestroy() {
+
+        if (!realm.isClosed)
+            realm.close()
+
+        super.onDestroy()
+    }
+
+    class FeedDataTask() : AsyncTask<Unit, Unit, Unit>() {
+
+        private val DATA_SIZE = 100000
+
+        override fun doInBackground(vararg params: Unit?) {
+            Realm.getDefaultInstance().use {
+
+                var list: ArrayList<RealmTestObject> = ArrayList<RealmTestObject>(DATA_SIZE)
+
+                (0..(DATA_SIZE - 1)).mapTo(list) { RealmTestObject(it.toLong(), "Test String $it") }
+
+                it.executeTransaction { realm -> realm.insert(list) }
+            }
+        }
     }
 }
