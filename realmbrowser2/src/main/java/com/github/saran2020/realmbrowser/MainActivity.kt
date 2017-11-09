4e43e6cd3e21b49a48e5ddefe90fefd64d700a0e
Saran Sankaran <saran.sankaran@gmail.com>
2017-11-09 20:10:46 +0530

    Testing the working of library module
    Signed-off-by: Saran Sankaran <saran.sankarn@gmail.com>

--- realmbrowser2/src/main/java/com/github/saran2020/realmbrowser/MainActivity.kt
+++ realmbrowser2/src/main/java/com/github/saran2020/realmbrowser/MainActivity.kt
@@ -2,15 +2,17 @@ package com.github.saran2020.realmbrowser
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import io.realm.Realm
 
 /**
  * Created by Saran Sankaran on 11/9/17.
  */
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        var realm = Realm.getDefaultInstance()
 
     }
 }
