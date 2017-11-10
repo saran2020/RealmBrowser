1f68a7e26fd759029d37d902038d81ec309f4cee
Saran Sankaran <saran.sankarn@gmail.com>
2017-11-10 15:45:26 +0530

    Updated: Used Result Activity to show result
    Signed-off-by: Saran Sankaran <saran.sankarn@gmail.com>

--- realmbrowser/src/main/java/com/github/saran2020/realmbrowser/MainActivity.kt
+++ realmbrowser/src/main/java/com/github/saran2020/realmbrowser/MainActivity.kt
@@ -2,12 +2,9 @@ package com.github.saran2020.realmbrowser
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
 import io.realm.Realm
-import io.realm.RealmModel
 
 /**
  * Created by Saran Sankaran on 11/9/17.
@@ -37,9 +34,7 @@ class MainActivity : AppCompatActivity() {
 
         buttonFetch.setOnClickListener {
             onFetchClick()
-            Toast.makeText(this, "Received Click", Toast.LENGTH_SHORT).show()
         }
-
 
     }
 
@@ -58,21 +53,9 @@ class MainActivity : AppCompatActivity() {
         var packageName = editPackageName.text
         var className = editClassName.text
 
-        try {
+        var fullClassName = packageName.append(className).toString()
 
-            var fullClassName = packageName.append(className).toString()
-            var classTest = Class.forName(fullClassName) as Class<RealmModel>
-            var test = realm.where<RealmModel>(classTest).findAll()
-            Log.d(TAG, "Size: " + test.size)
-        } catch (e: ClassNotFoundException) {
-
-            //TODO: class not found (Show appropriate message)
-            e.printStackTrace()
-        } catch (e: NullPointerException) {
-
-            //TODO: Is not Realm Model class (Show appropriate message)
-            e.printStackTrace()
-        }
+        ResultActivity.startActivity(this, fullClassName)
 
     }
 }
