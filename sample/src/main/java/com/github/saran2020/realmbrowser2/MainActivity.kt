aee131b27199fd558cac232df1c873f47ec85a7e
saran2020 <saran.sankaran@gmail.com>
2017-12-24 15:07:08 +0530

    REMOVED: Removed sample model to update new model
    Signed-off-by: saran2020 <saran.sankaran@gmail.com>

--- sample/src/main/java/com/github/saran2020/realmbrowser2/MainActivity.kt
+++ sample/src/main/java/com/github/saran2020/realmbrowser2/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import io.realm.Realm
-import io.realm.RealmList
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,12 +16,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         realm = Realm.getDefaultInstance()
-        var result = realm.where(MyRealmTestObject::class.java).findAll()
-
-        if (result.size == 0)
-            FeedDataTask(TAG).execute()
-
-        Log.d(TAG, "Size: ${result.size}")
     }
 
     override fun onDestroy() {
@@ -39,28 +32,6 @@ class MainActivity : AppCompatActivity() {
 
         override fun doInBackground(vararg params: Unit?) {
             Realm.getDefaultInstance().use {
-
-                var list: ArrayList<MyRealmTestObject> = ArrayList<MyRealmTestObject>(DATA_SIZE)
-                var listId: Long = 0
-                var singleItemId = 0
-
-                for (i in 0 until DATA_SIZE) {
-                    var listItems: RealmList<RealmListItem> = RealmList()
-
-                    for (id in listId until (listId + 5)) {
-                        listItems.add(RealmListItem(id, "List item with Id $listId"))
-                    }
-
-                    var singleItem = RealmSingleItem(singleItemId.toLong(), "Single item with Id $singleItemId")
-
-                    list.add(MyRealmTestObject(i.toLong(), "MyRealmTestObject with id $i", listItems, singleItem))
-
-                    singleItemId++
-                    listId += 5
-                }
-
-                it.executeTransaction { realm -> realm.insert(list) }
-
                 Log.d(TAG, "Completed..")
 
             }
