4b15bcd10a5858e4eceb4987c1ed2166730d1926
saran2020 <saran.sankaran@gmail.com>
2017-12-24 20:16:35 +0530

    ADDED: Added schema version for migration if required
    Signed-off-by: saran2020 <saran.sankaran@gmail.com>

--- sample/src/main/java/com/github/saran2020/realmbrowser2/Application.kt
+++ sample/src/main/java/com/github/saran2020/realmbrowser2/Application.kt
@@ -2,6 +2,7 @@ package com.github.saran2020.realmbrowser2
 
 import android.app.Application
 import io.realm.Realm
+import io.realm.RealmConfiguration
 
 /**
  * Created by Saran Sankaran on 11/9/17.
@@ -11,5 +12,9 @@ class Application : Application() {
     override fun onCreate() {
         super.onCreate()
         Realm.init(this)
+
+        val realmConfig = RealmConfiguration.Builder()
+                .name("sample.realm").schemaVersion(1).build()
+        Realm.setDefaultConfiguration(realmConfig)
     }
 }
