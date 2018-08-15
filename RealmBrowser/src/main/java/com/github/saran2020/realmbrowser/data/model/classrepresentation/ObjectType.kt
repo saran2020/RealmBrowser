e7e4a73c82ed6b5fbbe7f290606dab8ea63060d0
saran <saran.sankaran@gmail.com>
2018-08-15 18:54:17 +0530

    Added two classes missed in previous commit

---
+++ RealmBrowser/src/main/java/com/github/saran2020/realmbrowser/data/model/classrepresentation/ObjectType.kt
@@ -0,0 +1,34 @@
+package com.github.saran2020.realmbrowser.data.model.classrepresentation
+
+import com.github.saran2020.realmbrowser.data.model.ObjectInfo
+
+/**
+ * Whenever any model contains a field type of Object(Instance of RealmObject or RealmModel) instead
+ * of storing the exact value, we store this value instead. This will help us in handling the click on
+ * this item in table view
+ *
+ * ctor
+ * @param displayText The text that will be displayed in table view
+ * @param parentPrimaryKeyFieldName Stores the parents primary key field name. This will be used
+ * to data when the object is clicked in table view and we have to display the data.
+ * @param parentPrimaryKeyFieldName Primary key type of parent
+ * @param parentPrimaryKeyValue Value of primary key of parent, this will be used to display the
+ * data when this item is clicked in table view
+ *
+ * Created by Saran on 13-Jan-18.
+ */
+data class ObjectType private constructor(val displayText: String) {
+
+    lateinit var objectInfo: ObjectInfo
+
+    constructor(displayText: String,
+                parentClassName: String,
+                parentPrimaryKeyFieldName: String,
+                parentPrimaryKeyType: Int,
+                parentPrimaryKeyValue: Any?,
+                fieldName: String,
+                fieldGetterName: String,
+                fieldType: Int) : this(displayText) {
+        objectInfo = ObjectInfo(parentClassName, parentPrimaryKeyFieldName, parentPrimaryKeyType, parentPrimaryKeyValue, fieldName, fieldGetterName, fieldType)
+    }
+}
