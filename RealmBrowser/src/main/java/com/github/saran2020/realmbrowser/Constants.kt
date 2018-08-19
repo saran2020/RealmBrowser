b03c880dd22371a5062cbd1d7cebbabb1735c729
saran <saran.sankaran@gmail.com>
2018-08-19 17:08:33 +0530

    Removed some unwanted consts

--- RealmBrowser/src/main/java/com/github/saran2020/realmbrowser/Constants.kt
+++ RealmBrowser/src/main/java/com/github/saran2020/realmbrowser/Constants.kt
@@ -37,18 +37,6 @@ const val FIND_FIRST_STRING = "findFirst()"
 
 // data types
 const val NO_DATA_TYPE: Byte = 0
-const val TYPE_BOOLEAN: Byte = -1
-const val TYPE_BYTE: Byte = -2
-const val TYPE_CHAR: Byte = -3
-const val TYPE_SHORT: Byte = -4
-const val TYPE_INT: Byte = -5
-const val TYPE_LONG: Byte = -6
-const val TYPE_FLOAT: Byte = -7
-const val TYPE_DOUBLE: Byte = -8
-const val TYPE_STRING: Byte = -9
-const val TYPE_BYTE_ARRAY: Byte = -10
-const val TYPE_REALM_LIST: Byte = -11
-const val TYPE_REALM_OBJECT: Byte = -12
 
 const val GETTER_PREFIX = "get"
 const val BOOLEAN_GETTER_PREFIX = "is"
