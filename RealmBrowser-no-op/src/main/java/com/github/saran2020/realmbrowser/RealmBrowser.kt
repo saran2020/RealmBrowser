5920ddcb5e017d784c94d8a4ae498bb53bfe0130
saran <saran.sankaran@gmail.com>
2018-08-27 23:51:31 +0530

    Added a new no-op package

---
+++ RealmBrowser-no-op/src/main/java/com/github/saran2020/realmbrowser/RealmBrowser.kt
@@ -0,0 +1,15 @@
+package com.github.saran2020.realmbrowser
+
+import android.content.Context
+
+class RealmBroswer{
+    companion object {
+        fun showStartNotif(context: Context) {
+            // NO OP function for release version
+        }
+
+        fun start(context: Context) {
+            // NO OP function for release version
+        }
+    }
+}
